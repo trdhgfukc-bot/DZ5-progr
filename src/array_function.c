171ecf3c3aa6b269d4f9ef1bdd57c66e068b2640
S <trdhgfukc@gmail.com>
2025-10-16 23:27:44 +0700

    hw5

--- src/array_function.c
+++ src/array_function.c
@@ -7,7 +7,7 @@ void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, i
         dest[i] = src1[i];
     }
     for (int i = size_src1; i < size_src1+size_src2; ++i){
-        dest[i]=scr2[i-size_src1];
+        dest[i]=src2[i-size_src1];
     }
 }
 int comp(const void *a, const void *b){
@@ -19,7 +19,7 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
         dest[i] = src1[i];
     }
     for (int i = size_src1; i < size_src1+size_src2; ++i){
-        dest[i]=scr2[i-size_src1];
+        dest[i]=src2[i-size_src1];
     }
     int n = sizeof(dest)/sizeof(dest[0]);
     qsort(dest, n, sizeof(dest[0]), comp);
