cb9632d75b577d1f28b4d3dc2b3fbee1cb553635
S <trdhgfukc@gmail.com>
2025-10-16 23:46:00 +0700

    hw5

--- src/array_function.c
+++ src/array_function.c
@@ -7,7 +7,6 @@ int comp(const void *a, const void *b){
     return (*(int *)a - *(int *)b);
 }
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
-    int dest[size_src1+size_src2];
     for (int i = 0; i<size_src1; ++i){
         dest[i] = src1[i];
     }
