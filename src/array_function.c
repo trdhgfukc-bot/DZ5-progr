348e1cad220193bbe3ae0c95039cf166b50dd674
S <trdhgfukc@gmail.com>
2025-10-16 23:30:08 +0700

    hw5

--- src/array_function.c
+++ src/array_function.c
@@ -10,34 +10,3 @@ void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, i
         dest[i]=src2[i-size_src1];
     }
 }
-
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
-    int dest[size_src1+size_src2];
-    for (int i = 0; i<size_src1; ++i){
-        dest[i] = src1[i];
-    }
-    for (int i = size_src1; i < size_src1+size_src2; ++i){
-        dest[i]=src2[i-size_src1];
-    }
-    int n = sizeof(dest)/sizeof(dest[0]);
-    qsort(dest, n, sizeof(dest[0]), comp);
-    return dest;
-
-}
-
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
-    int timemin = 99999999999999999999999999;
-    int timemax = 0;
-    for (int i = 0; i < size; ++i){
-        if (src[i] > timemax) {
-            timemax = src[i];
-        }
-        if (src[i] < timemin) {
-            timemin = src[i];   
-        }
-    }
-    min = &timemin;
-    max = &timemax;
-
-    return min, max;
-}
