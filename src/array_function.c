420e81b458cfe5c351664427a03c8cd41ef4441e
S <trdhgfukc@gmail.com>
2025-10-16 23:32:56 +0700

    hw5

--- src/array_function.c
+++ src/array_function.c
@@ -2,11 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
+
+int comp(const void *a, const void *b){
+    return (*a - *b);
+}
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    int dest[size_src1+size_src2];
     for (int i = 0; i<size_src1; ++i){
         dest[i] = src1[i];
     }
     for (int i = size_src1; i < size_src1+size_src2; ++i){
         dest[i]=src2[i-size_src1];
     }
+    int n = sizeof(dest)/sizeof(dest[0]);
+    qsort(dest, n, sizeof(dest[0]), comp);
+    return dest;
+
 }
+
